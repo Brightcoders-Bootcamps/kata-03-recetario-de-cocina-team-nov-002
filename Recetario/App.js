e66b608a9020d9b6d78d9f9eeff30e4981a4097c
GracielaJazmin <gracielajazmin_chavez@ucol.mx>
2020-12-21 09:50:18 -0600

    react-native init

---
+++ Recetario/App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View
+} from 'react-native';
+
+import Buscar from './Saludar';
+
+
+function App() {
+  return (
+    <SafeAreaView>
+          <Buscar/>
+    </SafeAreaView>
+     
+  );
+};
+
+// const styles = StyleSheet.create({
+// Saludar:{
+//   flex: 1, 
+//   justifyContent: 'space-between', 
+//   alignItems: 'center'
+// }
+// });
+
+export default App;
