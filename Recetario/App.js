ce63b3b1df9e7804d282a1924af33eb77bfa4951
jjcr11 <jjcr0397@gmail.com>
2020-12-21 10:46:58 -0600

    Search bar

--- Recetario/App.js
+++ Recetario/App.js
@@ -1,36 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  View
-} from 'react-native';
+//import {SearchBar} from 'react-native-elements';
+import Bar from './Components/Bar';
+import List from './Components/List';
 
-import Buscar from './Saludar';
-
-
-function App() {
+const Main = () => {
   return (
-    <SafeAreaView>
-          <Buscar/>
-    </SafeAreaView>
-     
+    <View>
+      <Bar />
+      <List />
+    </View>
   );
 };
 
-// const styles = StyleSheet.create({
-// Saludar:{
-//   flex: 1, 
-//   justifyContent: 'space-between', 
-//   alignItems: 'center'
-// }
-// });
-
-export default App;
+export default Main;
---
+++ Recetario/Components/Bar.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import {SearchBar} from 'react-native-elements';
+
+const Bar = () => {
+  const [search, setSearch] = useState('');
+  return (
+    <SearchBar
+      placeholder="Type Here..."
+      onChangeText={setSearch}
+      value={search}
+    />
+  );
+};
+
+export default Bar;
