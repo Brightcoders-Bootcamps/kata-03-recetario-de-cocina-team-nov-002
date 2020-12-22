2e3995f7562b770e25f098bbf3a3e78c3b43037c
vmagana3 <vmagana3@ucol.mx>
2020-12-22 12:36:38 -0600

    Main view complete

--- Recetario/App.js
+++ Recetario/App.js
@@ -1,32 +1,58 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, StatusBar, Text} from 'react-native';
 import Bar from './Components/Bar';
-import List from './Components/List';
+import ListTrending from './Components/ListTrending';
+
+const DATA = [
+  {
+    id: '1',
+    title: 'Galletas',
+    image: require('./images/cookies.jpg') 
+  },
+  {
+    id: '2',
+    title: 'Cream Soup',
+    image: require('./images/creamSoup.jpg') 
+  },
+  {
+    id: '3',
+    title: 'Tacos',
+    image: require('./images/tacos.jpg')
+  },
+  {
+    id: '4',
+    title: 'Pizza',
+    image: require('./images/italianPizza.jpg')
+  },
+];
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Bar />
-      <Text>TRENDING</Text>
-      <List />
-      <Text>TRENDING</Text>
-      <Text>TRENDING</Text>
-      <Text>TRENDING</Text>
-      <Text>TRENDING</Text>
-      <Text>TRENDING</Text>
-      <Text>TRENDING</Text>
-      <Text>TRENDING</Text>
-
-      <Text>TRENDING</Text>
+      <Text style={styles.text1}>TRENDING</Text>
+      <ListTrending data={DATA}/>
+      <Text style={styles.text1}>RECENTS</Text>
+      <ListTrending data={DATA}/>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
+    container:
+    {      
+      padding:4,
+      backgroundColor:'gray',
+      flex:1      
+    },
+    text1:
+    {
+      fontSize:20,      
+      width:300,
+      marginLeft:10,
+      marginTop:15,
+      color: 'red'
+    }
 });
 
 export default App;
--- Recetario/Components/Bar.js
+++ Recetario/Components/Bar.js
@@ -1,16 +1,26 @@
 
 import React, {useState} from 'react';
 import {SearchBar} from 'react-native-elements';
+import {StyleSheet} from 'react-native';
 
 const Bar = () => {
   const [search, setSearch] = useState('');
   return (
     <SearchBar
-      placeholder="Type Here..."
+      styles={styles.searchBar}
+      placeholder="Busca Algo"
       onChangeText={setSearch}
       value={search}
     />
   );
 };
+
+const styles = StyleSheet.create({
+  searchBar:
+  {
+    borderRadius:10       
+  }
+
+})
 
 export default Bar;
