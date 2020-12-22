3dc58101544f0aca8c6a7de23a9edf55a14317b4
jjcr11 <jjcr0397@gmail.com>
2020-12-22 10:15:37 -0600

    First list item

--- Recetario/App.js
+++ Recetario/App.js
@@ -1,16 +1,32 @@
-import React, { Component, useState } from 'react';
-import {View, Text} from 'react-native'
+import React from 'react';
+import {SafeAreaView, StyleSheet, StatusBar, Text} from 'react-native';
 import Bar from './Components/Bar';
 import List from './Components/List';
 
 const App = () => {
-  const [search, setSearch] = useState('');
   return (
-    <View>
+    <SafeAreaView style={styles.container}>
       <Bar />
+      <Text>TRENDING</Text>
       <List />
-    </View>
+      <Text>TRENDING</Text>
+      <Text>TRENDING</Text>
+      <Text>TRENDING</Text>
+      <Text>TRENDING</Text>
+      <Text>TRENDING</Text>
+      <Text>TRENDING</Text>
+      <Text>TRENDING</Text>
+
+      <Text>TRENDING</Text>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+});
 
 export default App;
--- Recetario/Components/List.js
+++ Recetario/Components/List.js
@@ -1,38 +1,5 @@
-/*import React from 'react';
-import {ListItem, Avatar,View} from 'react-native-elements';
-
-const list = [
-  {
-    name: 'Amy Farha',
-    subtitle: 'Vice President',
-  },
-  {
-    name: 'Chris Jackson',
-    subtitle: 'Vice Chairman',
-  }
-];
-
-  <View>
-  {list.map((l, i) => (
-    <ListItem key={i} bottomDivider>
-      <ListItem.Content>
-        <ListItem.Title>{l.name}</ListItem.Title>
-        <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
-      </ListItem.Content>
-    </ListItem>
-  ))}
-</View>
-
-
-
-
-
-
-export default List
-*/   
-
 import React from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
+import {View, FlatList, StyleSheet, Text, StatusBar} from 'react-native';
 
 const DATA = [
   {
@@ -49,38 +16,32 @@ const DATA = [
   },
 ];
 
-const Item = ({ title }) => (
+const Item = ({title}) => (
   <View style={styles.item}>
     <Text style={styles.title}>{title}</Text>
   </View>
 );
 
 const List = () => {
-  const renderItem = ({ item }) => (
-    <Item title={item.title} />
-  );
+  const renderItem = ({item}) => <Item title={item.title} />;
 
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
-      />
-    </SafeAreaView>
+    <FlatList
+      horizontal={true}
+      data={DATA}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+    />
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
   item: {
     backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
+    paddingHorizontal: 1,
+    paddingVertical: 1,
+    marginVertical: 1,
+    marginHorizontal: 1,
   },
   title: {
     fontSize: 32,
